4aac4afb76107a25a3c0b998bda8cefe65c4be49
code with rizki <mrizki.nurrohman10@gmail.com>
2025-02-20 18:03:04 +0700

    minitask3 week2

---
+++ satu.js
@@ -0,0 +1,32 @@
+// dense ranking , sistem ranking
+// nilai tertinggi menjadi rank 1
+// nilai yg sama mendapat rank yang sama
+// nilai beda mendapat rank selanjutnya
+
+// memiliki 2 parameter
+
+// output berupa rank nilai yang masuk array of objek
+
+// bandingin nilai terbesar di input 1
+// bandingin input 2 dengan nilai input 1
+const denseRanking = (c, d) => {
+    
+
+        for (let i= 0; i <= c.length; i++) {
+            
+        }
+
+};
+
+denseRanking([100, 100, 50, 40, 40, 20, 10], [5, 25, 50, 120]);
+
+// function rankings(array) {
+//     return array
+//       .map((v, i) => [v, i])
+//       .sort((a, b) => b[0] - a[0])
+// //       .map((a, i) => [...a, i + 1])
+// //       .sort((a, b) => a[1] - b[1])
+// //       .map(a => a[2]);
+// }
+
+// console.log(rankings([10, 5, 20]));
